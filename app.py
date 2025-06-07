94bb3e1edba66eff37727c74def29617846eb2c1
Gitasri Das <gitasrid35@gmail.com>
2025-06-07 20:46:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
 
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
 
 ps = PorterStemmer()
 translator = Translator()
